f81c10b8bc2475cfc42fc86c09837a82ca0705af
shathipaul <shathipaul11@gmail.com>
2022-11-10 18:36:38 +0600

    added service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.send('paw care server is running');
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j6khnv2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -24,18 +25,49 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
-
+        app.post('/services', async(req, res) =>{
+            const result = await serviceCollection.insertOne(req.body);
+            res.send(result);
+        })
     }
     finally {
 
     }
+
+    try {
+        const serviceCollection = client.db('pawCare').collection('services')
+
+        app.get('/serviceslimit', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const limit = await cursor.limit(3).toArray();
+            res.send(limit);
+        })
+    }
+    finally {
+
+    }
+
+    try{
+        const serviceCollection = client.db('pawCare').collection('services')
+
+        app.get('/services/:id', async(req, res) =>{
+            const {id} = req.params;
+            const query = {_id: ObjectId(id)}
+            const servicesId = await serviceCollection.findOne(query);
+            res.send(servicesId);
+        })
+    }
+    finally{
+        
+    }
 }
 run().catch(error =>console.log(error));
 
+  
 
-app.get('/', (req, res) => {
-    res.send('paw care server is running');
-});
+
+
 
 app.listen(port, () => {
     console.log(`paw care running on: ${port}`);
